b0394ffc3371fcb9d97ff37233636f512c08b904
fgugnin22 <fedorgugnin22@gmail.com>
2024-04-13 12:05:15 +0300

    init.sql to create db structure

--- backend_vk/init.sql
+++ backend_vk/init.sql
@@ -1,3 +1,24 @@
 CREATE DATABASE IF NOT EXISTS task;
 
 USE task;
+
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE post (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT,
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES user(id)
+);
+
+CREATE TABLE reaction (
+    user_id INT,
+    post_id INT,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (post_id) REFERENCES post(id),
+    PRIMARY KEY (user_id, post_id)
+);
